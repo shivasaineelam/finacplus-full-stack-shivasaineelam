f2b3137945d521bd087704ee47fa376cfcd6a117
Your Name <your-email@example.com>
2025-03-04 16:23:34 +0530

    setup redux routing is done

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,9 +1,9 @@
 const express = require('express');
+const cors = require('cors');
 require('./config/env');
-const cookieParser = require('cookie-parser'); 
-const userRoutes=require('./routes/userRegistration')
-const otherroutes=require('./routes/otherroutes')
-
+const cookieParser = require('cookie-parser');
+const userRoutes = require('./routes/userRegistration');
+const otherroutes = require('./routes/otherroutes');
 
 const connectDatabase = require('./config/db');
 
@@ -12,15 +12,16 @@ const PORT = process.env.PORT || 5000;
 
 connectDatabase();
 
+app.use(cors());
 app.use(express.json());
-app.use(cookieParser()); 
-
+app.use(cookieParser());
 
 app.get('/', (req, res) => {
   res.send('hello world');
 });
-app.use("/api/v1/",userRoutes)
-app.use("/types",otherroutes);
+
+app.use("/api/v1/", userRoutes);
+app.use("/types", otherroutes);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
