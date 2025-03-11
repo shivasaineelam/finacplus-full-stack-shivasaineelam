67649909d5be1909dbdc96ccf200b2833fb47c39
Your Name <your-email@example.com>
2025-03-11 17:59:14 +0530

    added

--- frontend/src/hooks/useGenderDetails.js
+++ frontend/src/hooks/useGenderDetails.js
@@ -1,6 +1,7 @@
-import { useEffect } from "react";
+import {  useEffect } from "react";
 import { useDispatch } from "react-redux";
-import { addGenders } from "../utils/genderSlics";
+import { addGenders } from "../utlils/genderSlics.js";
+
 
 const useGenderDetails = () => {
   const dispatch = useDispatch();
